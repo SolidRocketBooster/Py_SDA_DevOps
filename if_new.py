a030f76148a7457f06decdb68f47f92662de91be
Olek <aleksander.wojtal@tieto.pl>
2019-03-02 11:51:22 +0100

    ok

---
+++ if_new.py
@@ -0,0 +1,9 @@
+liczba = int(input('Wprowadz liczbe: '))
+
+if liczba > 2:
+    print('Liczba jest wieksza niz 2')
+elif liczba == 2:
+    print('Liczba jest rowan 2')
+else:
+    print('Liczba jest miniejsza od 2')
+
---
+++ rps.py
@@ -0,0 +1,8 @@
+userChoice = input('Rock, paper or scissors?: ')
+
+if userChoice == 'paper':
+    print('Draw!')
+elif userChoice == 'rock':
+    print('You lost!')
+else:
+    print('You won!')
